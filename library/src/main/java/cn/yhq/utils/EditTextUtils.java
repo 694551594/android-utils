540449491b0f57d0fe07729b1af82d5a8f32fd9a
yanghuiqiang <694551594@qq.com>
2017-02-08 15:53:47 +0800

    增加依赖库。

---
+++ library/src/main/java/cn/yhq/utils/EditTextUtils.java
@@ -0,0 +1,17 @@
+package cn.yhq.utils;
+
+import android.text.Selection;
+import android.text.Spannable;
+import android.widget.EditText;
+
+public class EditTextUtils {
+
+    public static void onTextToEnd(EditText editText) {
+        CharSequence text = editText.getText();
+        if (text instanceof Spannable) {
+            Spannable spanText = (Spannable) text;
+            Selection.setSelection(spanText, text.length());
+        }
+    }
+
+}
--- library/src/main/java/cn/yhq/utils/JsonUtils.java
+++ library/src/main/java/cn/yhq/utils/JsonUtils.java
@@ -7,39 +7,41 @@ import com.google.gson.Gson;
 import java.lang.reflect.Type;
 
 public class JsonUtils {
-  public static String toJson(Object o) {
-    if (o == null) {
-      return null;
-    }
-    try {
-      return new Gson().toJson(o);
-    } catch (Exception e) {
-      e.printStackTrace();
-      return "";
-    }
-  }
 
-  public static <T> T toObject(String json, Class<T> classOfT) {
-    if (TextUtils.isEmpty(json)) {
-      return null;
+    public static String toJson(Object o) {
+        if (o == null) {
+            return null;
+        }
+        try {
+            return new Gson().toJson(o);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
     }
-    try {
-      return new Gson().fromJson(json, classOfT);
-    } catch (Exception e) {
-      e.printStackTrace();
-      return null;
-    }
-  }
 
-  public static <T> T toObject(String json, Type typeOfT) {
-    if (TextUtils.isEmpty(json)) {
-      return null;
+    public static <T> T toObject(String json, Class<T> classOfT) {
+        if (TextUtils.isEmpty(json)) {
+            return null;
+        }
+        try {
+            return new Gson().fromJson(json, classOfT);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
-    try {
-      return new Gson().fromJson(json, typeOfT);
-    } catch (Exception e) {
-      e.printStackTrace();
-      return null;
+
+    public static <T> T toObject(String json, Type typeOfT) {
+        if (TextUtils.isEmpty(json)) {
+            return null;
+        }
+        try {
+            return new Gson().fromJson(json, typeOfT);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
-  }
+
 }
